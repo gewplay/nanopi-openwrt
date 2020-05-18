55943f67e333170a56f609e40d6249a86a031565
gewplay <48548545+gewplay@users.noreply.github.com>
2020-05-18 14:19:11 +0800

    Update autoupdate.sh

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -16,12 +16,12 @@ case "$num" in
 esac
 cd /mnt/mmcblk0p2
 rm -rf artifact R2S*.zip FriendlyWrt*img*
-wget https://github.com/gewplay/nanopi-openwrt/releases/download/R2S-${fox}-$(date +%Y-%m-%d)/R2S-${fox}-$(date +%Y-%m-%d)-ROM.zip
+wget-ssl https://github.com/gewplay/nanopi-openwrt/releases/download/R2S-${fox}-$(date +%Y-%m-%d)/R2S-${fox}-$(date +%Y-%m-%d)-ROM.zip
 if [ -f /mnt/mmcblk0p2/R2S*.zip ]; then
 	echo -e '\e[92m今天固件已下载，准备解压\e[0m'
 else
 	echo -e '\e[91m今天的固件还没更新，尝试下载昨天的固件\e[0m'
-	wget https://github.com/gewplay/nanopi-openwrt/releases/download/R2S-${fox}-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)/R2S-${fox}-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)-ROM.zip
+	wget-ssl https://github.com/gewplay/nanopi-openwrt/releases/download/R2S-${fox}-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)/R2S-${fox}-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)-ROM.zip
 	if [ -f /mnt/mmcblk0p2/R2S*.zip ]; then
 		echo -e '\e[92m昨天的固件已下载，准备解压\e[0m'
 	else
